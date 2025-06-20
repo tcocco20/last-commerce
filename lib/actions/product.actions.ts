5673de660c07c7f96210c5d9bd902d69f6c2daa4
tcocco20 <theo@coccoscoding.dev>
2025-06-20 11:27:15 -0700

    Created ProductImages component

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -17,7 +17,23 @@ export async function getLatestProducts() {
   } catch (error) {
     console.error("Error fetching latest products:", error);
     throw error;
-  } finally {
-    await prisma.$disconnect();
+  }
+}
+
+// get single product by slug
+export async function getProductBySlug(slug: string) {
+  try {
+    const data = await prisma.product.findFirst({
+      where: {
+        slug,
+      },
+    });
+    if (!data) {
+      throw new Error("Product not found");
+    }
+    return convertToPlainObject(data);
+  } catch (error) {
+    console.error("Error fetching product by slug:", error);
+    throw error;
   }
 }
