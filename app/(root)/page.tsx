dacfba8351bd9ed28177ac3c7d37d30464759eff
tcocco20 <theo@coccoscoding.dev>
2025-06-20 11:43:40 -0700

    trying type casting to fix error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
 import ProductList from "@/components/shared/Product/ProductList";
-import sampleData from "@/db/sample-data";
 import { getLatestProducts } from "@/lib/actions/product.actions";
+import { Product } from "@/lib/types";
 
 const HomePage = async () => {
-  const latestProduct = await getLatestProducts();
+  const latestProduct = (await getLatestProducts()) as Product[];
   return (
     <>
       <ProductList data={latestProduct} title="Newest Arrivals" limit={4} />
