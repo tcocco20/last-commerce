650c0f9b506a3f459aa8cb7badf67f91639672c0
tcocco20 <theo@coccoscoding.dev>
2025-07-02 13:33:44 -0700

    started working on sign in page, and actions for sign in and log out, will work on hooking up page and action next

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -7,3 +7,8 @@ export const APP_URL =
 
 export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
+
+export const signInDefaultValues = {
+  email: "",
+  password: "",
+};
