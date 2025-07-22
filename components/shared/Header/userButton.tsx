3600786410e4130b173305f356211cf3e95fee03
tcocco20 <theo@coccoscoding.dev>
2025-07-22 16:37:46 -0700

    created user button for signing in and signing out then added it to menu.

---
+++ components/shared/Header/userButton.tsx
@@ -0,0 +1,70 @@
+import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { signOutUser } from "@/lib/actions/user.actions";
+import { UserIcon } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+const UserButton = async () => {
+  const session = await auth();
+
+  if (!session) {
+    return (
+      <Button asChild>
+        <Link href="/sign-in">
+          <UserIcon /> Sign In
+        </Link>
+      </Button>
+    );
+  }
+
+  const firstInitial = session.user?.name?.charAt(0).toUpperCase() ?? "U";
+
+  return (
+    <div className="flex gap-2 items-center">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <div className="flex items-center">
+            <Button
+              variant="ghost"
+              className="relative w-8 h-8 rounded-full ml-2 flex items-center justify-center bg-gray-200"
+            >
+              {firstInitial}
+            </Button>
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <div className="text-sm font-medium leading-none">
+                {session.user?.name || "User"}
+              </div>
+              <div className="text-sm leading-none text-muted-foreground">
+                {session.user?.email || "user@example.com"}
+              </div>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuItem className="p-0 mb-1">
+            <form action={signOutUser} className="w-full">
+              <Button
+                className="w-full py-4 px-2 h-4 justify-start cursor-pointer"
+                variant="ghost"
+              >
+                Sign Out
+              </Button>
+            </form>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default UserButton;
