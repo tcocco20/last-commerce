ad6aab0337e5b784bd408960ac6f9085533ce597
tcocco20 <theo@coccoscoding.dev>
2025-07-23 11:57:45 -0700

    corrected spelling in error messages

--- lib/validators.ts
+++ lib/validators.ts
@@ -9,13 +9,13 @@ const currency = z
   );
 
 export const insertProductSchema = z.object({
-  name: z.string().min(3, "Name mus be at least 3 characters long"),
-  slug: z.string().min(3, "Slug mus be at least 3 characters long"),
-  category: z.string().min(3, "Category mus be at least 3 characters long"),
-  brand: z.string().min(3, "Brand mus be at least 3 characters long"),
+  name: z.string().min(3, "Name must be at least 3 characters long"),
+  slug: z.string().min(3, "Slug must be at least 3 characters long"),
+  category: z.string().min(3, "Category must be at least 3 characters long"),
+  brand: z.string().min(3, "Brand must be at least 3 characters long"),
   description: z
     .string()
-    .min(3, "Description mus be at least 3 characters long"),
+    .min(3, "Description must be at least 3 characters long"),
   stock: z.coerce.number(),
   images: z
     .array(z.string().url("Invalid image URL"))
