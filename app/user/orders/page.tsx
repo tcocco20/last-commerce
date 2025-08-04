33ca6d57028405cb19df7affb500ebd1fd414abb
tcocco20 <theo@coccoscoding.dev>
2025-08-04 12:43:09 -0700

    created user orders table and pagination functionality

--- app/user/orders/page.tsx
+++ app/user/orders/page.tsx
@@ -1,7 +1,82 @@
-import React from "react";
+import Pagination from "@/components/shared/Pagination";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { getMyOrders } from "@/lib/actions/order.actions";
+import { formatCurrency, formatDateTime, formatId } from "@/lib/utils";
+import { Metadata } from "next";
+import Link from "next/link";
 
-const UserOrdersPage = () => {
-  return <div>UserOrdersPage</div>;
+export const metadata: Metadata = {
+  title: "My Orders",
+  description: "View your past orders and their details.",
+};
+
+const UserOrdersPage = async (props: {
+  searchParams: Promise<{ page?: string }>;
+}) => {
+  const { page } = await props.searchParams;
+
+  const orders = await getMyOrders({ page: page ? parseInt(page) : 1 });
+
+  return (
+    <>
+      <div className="space-y-2">
+        <h2 className="h2-bold">Orders</h2>
+        <div className="overflow-x-auto">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>ID</TableHead>
+                <TableHead>DATE</TableHead>
+                <TableHead>TOTAL</TableHead>
+                <TableHead>PAID</TableHead>
+                <TableHead>DELIVERED</TableHead>
+                <TableHead>ACTIONS</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {orders.data.map((order) => (
+                <TableRow key={order.id}>
+                  <TableCell>{formatId(order.id)}</TableCell>
+                  <TableCell>
+                    {formatDateTime(order.createdAt).dateTime}
+                  </TableCell>
+                  <TableCell>{formatCurrency(order.totalPrice)}</TableCell>
+                  <TableCell>
+                    {order.isPaid && order.paidAt
+                      ? formatDateTime(order.paidAt).dateTime
+                      : "Not Paid"}
+                  </TableCell>
+                  <TableCell>
+                    {order.isDelivered && order.deliveredAt
+                      ? formatDateTime(order.deliveredAt).dateTime
+                      : "Not Delivered"}
+                  </TableCell>
+                  <TableCell>
+                    <Link href={`/user/orders/${order.id}`}>
+                      <span className="px-2 text-blue-600">Details</span>
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          {orders.totalPages > 1 && (
+            <Pagination
+              totalPages={orders.totalPages}
+              currentPage={page ? Number(page) : 1}
+            />
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default UserOrdersPage;
