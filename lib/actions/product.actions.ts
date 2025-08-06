780a23b3a0310a328bc917f0589f777dd3b1237d
tcocco20 <theo@coccoscoding.dev>
2025-08-06 09:35:20 -0700

    added delete functionality and page for managing products.

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,8 +1,9 @@
 "use server";
 
 import { prisma } from "@/db/prisma";
-import { LATEST_PRODUCTS_LIMIT } from "../constants";
-import { convertToPlainObject } from "../utils";
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
+import { convertToPlainObject, formatError } from "../utils";
+import { revalidatePath } from "next/cache";
 
 // get latest products
 export async function getLatestProducts() {
@@ -35,5 +36,50 @@ export async function getProductBySlug(slug: string) {
   } catch (error) {
     console.error("Error fetching product by slug:", error);
     throw error;
+  }
+}
+
+export async function getAllProducts({
+  query,
+  limit = PAGE_SIZE,
+  page,
+  category,
+}: {
+  query: string;
+  limit?: number;
+  page: number;
+  category?: string;
+}) {
+  const data = await prisma.product.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
+  });
+
+  const dataCount = await prisma.product.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
+}
+
+export async function deleteProduct(id: string) {
+  try {
+    const productExists = await prisma.product.findFirst({
+      where: { id },
+    });
+
+    if (!productExists) {
+      throw new Error("Product not found");
+    }
+
+    await prisma.product.delete({
+      where: { id },
+    });
+
+    revalidatePath("/admin/products");
+    return { success: true, message: "Product deleted successfully" };
+  } catch (error) {
+    return { success: false, error: formatError(error) };
   }
 }
