30d5bd187527a1691d12df9d1ff384cc07e77774
tcocco20 <theo@coccoscoding.dev>
2025-08-09 16:39:31 -0700

    resolving deploy errs

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -15,18 +15,18 @@ import Link from "next/link";
 import React from "react";
 
 const AdminProductsPage = async (props: {
-  searchParams: Promise<{ page: string; query: string; category: string }>;
+  searchParams: Promise<{ page: string /*query: string; category: string*/ }>;
 }) => {
   const searchParams = await props.searchParams;
 
   const page = Number(searchParams.page) || 1;
-  const searchText = searchParams.query || "";
-  const category = searchParams.category || "";
+  // const searchText = searchParams.query || "";
+  // const category = searchParams.category || "";
 
   const products = await getAllProducts({
-    query: searchText,
+    // query: searchText,
     page,
-    category,
+    // category,
   });
 
   return (
